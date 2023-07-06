7cc4b2d50a97a1708039593583dcdef461fbc926
pwadmin <2924646170@qq.com>
2023-07-06 11:03:08 +0800

    修改删除了测试教学文件

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from 'antd'
-import Touxiang from './Touxiang'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,20 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-
-      <Touxiang />
-      <Touxiang />
-      <Touxiang />
-      <Touxiang />
-      <Button type="primary">Antd 的按钮</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -41,5 +33,3 @@ function App() {
 }
 
 export default App
-
-
