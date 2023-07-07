b80909c2f9ff894a8dd96a8ead0cb1aad47c28fe
pwadmin <2924646170@qq.com>
2023-07-07 22:28:41 +0800

    修改了login页面组件代码并删除项目自带样式css代码

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import Login from './pages/login/login.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,63 @@
 import "./login.css";
 import viteLogo from "/vite.svg";
-import { Button, Input } from "antd";
+import { Button, Form, Input } from "antd";
+// import { ForwardOutlined } from "@ant-design/icons";
 
 function Login() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
   return (
     <>
-      <div>
+      <div className="title">
         <a href="###" target="" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a>{" "}
+        <h1>Game管理后台系统</h1>
+        <Form className="form" layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "账号不能为空",
+              },
+            ]}
+          >
+            <Input
+              className="input"
+              size="large"
+              placeholder="请输入账号或手机号"
+            />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "密码不能为空",
+              },
+            ]}
+          >
+            <Input.Password
+              className="input"
+              size="large"
+              placeholder="请输入密码"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block size="large">
+              登录
+              {/* <ForwardOutlined /> */}
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-      <h1>Game管理后台系统</h1>
-      <Input className="input" placeholder="请输入账号或手机号" />
-      <Input className="input" placeholder="请输入密码" />
-
-      <Button type="primary">登录</Button>
     </>
   );
 }
+
 export default Login;
