264537171e0b0a9c4a116767187dff1d998d7a33
pwadmin <2924646170@qq.com>
2023-07-10 13:38:48 +0800

    新增了home页面引入了导航栏

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import Login from './pages/login/login.jsx'
+// import Login from './pages/login/login.jsx'
+import Home from './pages/home/home.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
+  // <React.StrictMode>
+  //   <Login />
+  // </React.StrictMode>,
   <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
+    <Home/>
+  </React.StrictMode>
 )
